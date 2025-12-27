cec3183375bc408f4a694861d897f6064be45a81
wnstj1548 <wnstj0614@naver.com>
2025-12-27 14:32:58 +0900

    feat : "router"

---
+++ Router/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from routes import item, user
+app = FastAPI()
+
+app.include_router(item.router)
+app.include_router(user.router)
---
+++ Router/routes/user.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/")
+async def read_users():
+    return [{"username": "Rickie"}, {"username": "Martin"}]
+
+
+@router.get("/me")
+async def read_user_me():
+    return {"username": "currentuser"}
+
+
+@router.get("/{username}")
+async def read_user(username: str):
+    return {"username": username}
