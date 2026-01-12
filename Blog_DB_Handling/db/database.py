746d3a51ca751eecfdc03138a878e2565fa517bc
김준서 <wnstj0614@naver.com>
2026-01-12 16:48:46 +0900

    feat : "환경설정 파일 분리"

--- Blog_DB_Handling/db/database.py
+++ Blog_DB_Handling/db/database.py
@@ -2,18 +2,16 @@ from sqlalchemy import create_engine, Connection
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.pool import QueuePool
-# from contextlib import contextmanager
 # from fastapi import status
 # from fastapi.exceptions import HTTPException
-# from dotenv import load_dotenv
-# import os
+from dotenv import load_dotenv
+import os
 
-DATABASE_CONN = "mysql+mysqlconnector://root:1548@localhost:3306/blog_db"
+# DATABASE_CONN = "mysql+mysqlconnector://root:1548@localhost:3306/blog_db"
 
-# load_dotenv()
-#
-# DATABASE_CONN = os.getenv("DATABASE_CONN")
-# print("########", DATABASE_CONN)
+load_dotenv()
+
+DATABASE_CONN = os.getenv("DATABASE_CONN")
 
 engine = create_engine(DATABASE_CONN, poolclass=QueuePool, pool_size=10, max_overflow=0, pool_recycle=300)
 
